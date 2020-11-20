d6ce29bbb08592ec2354257706fd4fa64e1b6cc4
Demchenko87 <conder@ukr.net>
2020-11-20 22:22:04 +0200

    image to txt

---
+++ main.py
@@ -0,0 +1,11 @@
+import pytesseract
+from PIL import Image
+img = Image.open('unnamed.jpg')
+file_name = img.filename
+file_name = file_name.split(".")[0]
+#custom_config = r'--oem 3 --psm 13'
+custom_config = r'--oem 3 --psm 6'
+text = pytesseract.image_to_string(img, lang='rus', config=custom_config)
+print(text)
+with open(f'{file_name}.txt', 'w') as  text_file:
+    text_file.write(text)
